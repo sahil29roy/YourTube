77835b741067db936890a2b87a4e1d137291c2c0
piyush-roy29 <roy29piyush@gmail.com>
2024-11-12 11:51:48 +0530

    add authUserBtn

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -32,6 +32,30 @@ const Navbar=()=> {
         <p className="appBox"></p>
         <p className="appBox"></p>
       </div>
+      <IoMdNotificationsOutline size={22} className={"vid_bell_Navbar"}/>
+      <div className="Auth_cont_Navbar">
+        {currentuser ? (
+          <>
+          
+          <div className="Channel_logo_App" onClick={()=>setauthbtn(true)}>
+            <p className="fstChar_logo_App">
+              {currentuser?.result.name ?(
+                <>{currentuser?.result.name.charAt(0).toUpperCase()}</>
+              ):(
+                <>{currentuser?.result.email.charAt(0).toUpperCase()}</>
+              ) }
+            </p>
+          </div>
+          </>
+        ):(
+          <>
+          <BiUserCircle size={22}/>
+          <b>Sign in</b>
+          </>
+        )
+
+        }
+      </div>
     </div>
   )
 }
