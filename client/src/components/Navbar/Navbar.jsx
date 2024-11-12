e18f99c9ceab65326c71b468dff6036e2e1239f0
piyush-roy29 <roy29piyush@gmail.com>
2024-11-12 10:45:52 +0530

    setup navbar div

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import logo from "./logo.ico"
+import "./Navbar.css"
 const Navbar=()=> {
+      const [authbtn,setauthbtn]= useState(false)
+      const currentuser = null;
   return (
-    <div>
+    <div className="Container_Navbar">
+      <div className="Burger_logo_Navbar">
+        <div className="burger" onClick={()=>toggledrawer()}>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <link></link>
+      </div>
       
     </div>
   )
