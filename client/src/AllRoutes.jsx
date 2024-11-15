7dab13ddeb459e20e044a18b8ef2d0978ada3745
piyush-roy29 <roy29piyush@gmail.com>
2024-11-15 10:53:07 +0530

    add home routes

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
 const AllRoutes = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<BiHome/>} />
+      </Routes>
     </div>
   )
 }
