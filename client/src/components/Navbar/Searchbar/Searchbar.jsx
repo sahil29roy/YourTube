db1b1e7bee94232a4224bec2ba8298b7eb90c40e
piyush-roy29 <roy29piyush@gmail.com>
2024-11-15 11:48:08 +0530

    create search bar directory

---
+++ client/src/components/Navbar/Searchbar/Searchbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Searchlist from './Searchlist';
+import { BsMicFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+const Searchbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Searchbar
