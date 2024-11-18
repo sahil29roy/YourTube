89d86fbf887a8dd49602a084fd29c873c428bee2
sahil29roy <sahil29roy@gmail.com>
2024-11-19 03:08:43 +0530

    add home navbarlink

--- client/src/components/Leftsidebar/Leftsidebar.jsx
+++ client/src/components/Leftsidebar/Leftsidebar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import "./Leftsidebar.css"
 import shorts from "./shorts.png"
-import {AiOutlineHome} from "react-icons/ai"
-import {MdOutlineExplore, MdOutlineSubscriptions, MdOutlineVideoLibrary} from "react-icons/md"
+import { AiOutlineHome } from "react-icons/ai"
+import { MdOutlineExplore, MdOutlineSubscriptions, MdOutlineVideoLibrary } from "react-icons/md"
 import { NavLink } from 'react-router-dom'
 const Leftsidebar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container_leftsidebar">
+        <NavLink to={'/'} className={"icon_sidebar"}>
+          <AiOutLineHome size={22} className='icon_sidebar' />
+          <div className="text_sidebar_icon">Home</div>
+        </NavLink>
+      </div>
+    </>
   )
 }
 
