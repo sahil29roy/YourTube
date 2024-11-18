b2cffa77838baa5a986f7018cf4a079c11efd035
piyush-roy29 <roy29piyush@gmail.com>
2024-11-19 01:48:11 +0530

    bugfix error searchbar

--- client/src/components/Navbar/Searchbar/Searchbar.jsx
+++ client/src/components/Navbar/Searchbar/Searchbar.jsx
@@ -1,30 +1,32 @@
-import React,{useState} from 'react'
-import Searchlist from './Searchlist';
-import { BsMicFill } from "react-icons/bs";
-import { FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import './Searchbar.css';
+import React, { useState } from 'react'
+import "./Searchbar.css"
+import { BsMicFill } from "react-icons/bs"
+import { FaSearch } from "react-icons/fa"
+import Searchlist from './Searchlist'
+
+import { Link } from 'react-router-dom'
 
 const Searchbar = () => {
-    const [Searchquery,setsearchquery] = useState("");
-    const [Searchlist,setsearchlist] = useState(false);
-    const Titlearray=  ["video1","video2","animated video","Movies"].filter(q=>q.toUpperCase().includes(Searchquery?.toUpperCase()))
-  return (
-    <>
-    <div className='searchBar_Container'>
-        <div className="searchBar_Container2">
-            <div className="search_div">
-                <input type="text" className='iBox_SearchBar' placeholder="Search" onChange={e=>setsearchquery(e.target.value)} value={Searchquery} onClick={e=>setsearchlist(true)}/>
-                <Link to={`/search/${Searchquery}`}>
-                <FaSearch className="searchIcon_SearchBar" />
-              </Link>
-                <BsMicFill callName='Mic_SearchBar'/>
-                {Searchquery && Searchlist && <Searchlist setsearchquery={setsearchquery} Titlearray={Titlearray}/>}
+    const [Searchquery, setsearchquery] = useState("")
+    const [searchlist, setsearchlist] = useState(false)
+     const Titlearray = ["video1", "video2", "animation video", "Movies"].filter(q => q.toUpperCase().includes(Searchquery.toUpperCase()))
+    return (
+        <>
+            <div className="SearchBar_Container">
+                <div className="SearchBar_Container2">
+                    <div className="search_div">
+                        <input type="text" className='iBox_SearchBar' placeholder='Search' onChange={e => setsearchquery(e.target.value)} value={Searchquery} onClick={e=>setsearchlist(true)}/>
+                        <Link to={`/search/${Searchquery}`}>
+                        <FaSearch className="searchIcon_SearchBar"/>
+                        </Link>
+                        <BsMicFill className='Mic_SearchBar'/>
+                        {Searchquery && searchlist && 
+                        <Searchlist setsearchquery={setsearchquery} Titlearray={Titlearray}/>}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
-export default Searchbar;
+export default Searchbar
