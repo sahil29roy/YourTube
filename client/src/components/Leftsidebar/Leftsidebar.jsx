93f38f3c19fd357fee64432c199bebea2c83798c
sahil29roy <sahil29roy@gmail.com>
2024-11-19 03:10:32 +0530

    add leftsidebar

--- client/src/components/Leftsidebar/Leftsidebar.jsx
+++ client/src/components/Leftsidebar/Leftsidebar.jsx
@@ -7,12 +7,28 @@ import { NavLink } from 'react-router-dom'
 const Leftsidebar = () => {
   return (
     <>
-      <div className="container_leftsidebar">
-        <NavLink to={'/'} className={"icon_sidebar"}>
-          <AiOutLineHome size={22} className='icon_sidebar' />
-          <div className="text_sidebar_icon">Home</div>
+      <div className="container_leftSidebar">
+        <NavLink to={'/'} className="icon_sidebar_div">
+            <AiOutlineHome size={22} className='icon_sidebar'/>
+            <div className="text_sidebar_icon">Home</div>
         </NavLink>
-      </div>
+        <div className="icon_sidebar_div">
+            <MdOutlineExplore size={22} className='icon_sidebar'/>
+            <div className="text_sidebar_icon">Explore</div>
+        </div>
+        <div className="icon_sidebar_div">
+            <img src={shorts} width={22} className='icon_sidebar'/>
+            <div className="text_sidebar_icon">Shorts</div>
+        </div>
+        <div className="icon_sidebar_div">
+            <MdOutlineSubscriptions size={22} className='icon_sidebar'/>
+            <div className="text_sidebar_icon" style={{fontSize:"12px"}}>Subscription</div>
+        </div>
+        <NavLink to={'/Library'} className="icon_sidebar_div">
+            <MdOutlineVideoLibrary size={22} className='icon_sidebar'/>
+            <div className="text_sidebar_icon">Library</div>
+        </NavLink>
+    </div>
     </>
   )
 }
