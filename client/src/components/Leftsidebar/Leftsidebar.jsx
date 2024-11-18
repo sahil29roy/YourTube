b03a617c06c86bd28b95a348ff557827fc799406
sahil29roy <sahil29roy@gmail.com>
2024-11-19 03:04:30 +0530

    import all necessary statements

--- client/src/components/Leftsidebar/Leftsidebar.jsx
+++ client/src/components/Leftsidebar/Leftsidebar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import "./Leftsidebar.css"
+import shorts from "./shorts.png"
+import {AiOutlineHome} from "react-icons/ai"
+import {MdOutlineExplore, MdOutlineSubscriptions, MdOutlineVideoLibrary} from "react-icons/md"
+import { NavLink } from 'react-router-dom'
 const Leftsidebar = () => {
   return (
     <div>
