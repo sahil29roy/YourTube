672ad7980520842d88d5ece7c8fc2992de9e155e
sahil29roy <sahil29roy@gmail.com>
2024-12-30 11:51:50 +0530

    add navlist array

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import Leftsidebar from '../../components/Leftsidebar/Leftsidebar'
 import "./Home.css"
 const Home = ()=>{
+  const navlist = [
+    "All",
+    "Python",
+    "Java",
+    "C++",
+    "Movies",
+    "Science",
+    "Animation",
+    "Gaming",
+    "Comedy"
+  ];
   return (
     <div>
       <div className="container_Pages_App">
