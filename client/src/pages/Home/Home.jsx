5e90b71defd80ab5ce55555b1859ba3652a2aa1e
sahil29roy <sahil29roy@gmail.com>
2024-12-30 11:54:28 +0530

    add navlist

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -17,6 +17,15 @@ const Home = ()=>{
     <div>
       <div className="container_Pages_App">
       <Leftsidebar/>
+      <div className="container2_pages_App">
+        <div className="navigation_Home">
+          {navlist.map((m)=>{
+            return(
+              <p key={m} className='btn_nav_home'>{m}</p>
+            );
+          })}
+        </div>
+      </div>
       </div>
     </div>
   )
