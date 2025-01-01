7dde2a34011f4c9c108cbc7222a27ecf83b0b990
sahil29roy <sahil29roy@gmail.com>
2025-01-01 23:23:24 +0530

    import necessary icons

--- client/src/components/Leftsidebar/Drawersliderbar.jsx
+++ client/src/components/Leftsidebar/Drawersliderbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import "./Leftsidebar.css"
+import { AiFillPlaySquare, AiOutlineHome, AiFillLike } from 'react-icons/ai'
+import { MdOutlineExplore, MdOutlineVideoLibrary, MdSubscriptions, MdOutlineWatchLater } from "react-icons/md"
+import { FaHistory } from 'react-icons/fa'
 const Drawersliderbar = () => {
   return (
     <div>
