76205939bcd6b901b1f49f339858c8962dec25dd
sahil29roy <sahil29roy@gmail.com>
2025-01-02 00:45:27 +0530

    add home,explore,shots navigation

--- client/src/components/Leftsidebar/Drawersliderbar.jsx
+++ client/src/components/Leftsidebar/Drawersliderbar.jsx
@@ -10,8 +10,32 @@ const Drawersliderbar = ({ toggledraw, toggledrawersidebar }) => {
     <div>
       <div className="container_DrawaerLeftSidebar" style={toggledrawersidebar}>
       <div className="container2_DrawaerLeftSidebar">
-        <div className="Drawer_leftsidebar">
+      <div className="Drawer_leftsidebar">
+          <NavLink to={'/'} className="icon_sidebar_div">
+            <p>
+              <AiOutlineHome size={22} className='icon_sidebar' style={{ margin: "auto 0.7rem" }} />
+              <div className="text_sidebar_icon">Home</div>
+            </p>
+          </NavLink>
+          <div className="icon_sidebar_div">
+            <p>
+              <MdOutlineExplore size={22} className='icon_sidebar' style={{ margin: "auto 0.7rem" }} />
+              <div className="text_sidebar_icon">Explore</div>
+            </p>
+          </div>
+          <div className="icon_sidebar_div">
+            <p>
+              <img src={shorts} width={22} className='icon_sidebar' style={{ margin: "auto 0.7rem" }} />
+              <div className="text_sidebar_icon">Shorts</div>
+            </p>
+          </div>
 
+          <div className="icon_sidebar_div">
+            <p>
+              <MdSubscriptions size={22} className='icon_sidebar' style={{ margin: "auto 0.7rem" }} />
+              <div className="text_sidebar_icon">Subscriptions</div>
+            </p>
+          </div>
         </div>
         </div>
         </div>
