8905724773322b9c256a1f7f322b5df9d7c5d727
sahil29roy <sahil29roy@gmail.com>
2025-01-01 23:58:44 +0530

    add props to drawer slider bar

--- client/src/components/Leftsidebar/Drawersliderbar.jsx
+++ client/src/components/Leftsidebar/Drawersliderbar.jsx
@@ -3,7 +3,9 @@ import "./Leftsidebar.css"
 import { AiFillPlaySquare, AiOutlineHome, AiFillLike } from 'react-icons/ai'
 import { MdOutlineExplore, MdOutlineVideoLibrary, MdSubscriptions, MdOutlineWatchLater } from "react-icons/md"
 import { FaHistory } from 'react-icons/fa'
-const Drawersliderbar = () => {
+import shorts from "./shorts.png"
+import { NavLink } from 'react-router-dom'
+const Drawersliderbar = ({ toggledraw, toggledrawersidebar }) => {
   return (
     <div>
       
