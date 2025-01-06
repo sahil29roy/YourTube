a7e66e8752b54d96282d90ca0fc6586bd759978a
sahil29roy <sahil29roy@gmail.com>
2025-01-06 20:14:05 +0530

    add show video grid

--- client/src/components/Showvideogrid/Showvideogrid.jsx
+++ client/src/components/Showvideogrid/Showvideogrid.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
-function Showvideogrid() {
+import "./Showvideogrid.css"
+import Showvideo from '../Showvideo/Showvideo'
+const Showvideogrid = ({ vid }) => {
   return (
-    <div>
-      
+    <div className="Container_ShowVideoGrid">
+      {
+        vid?.reverse().map(vi => {
+          return (
+            <div key={vi._id} className="video_box_app">
+              <Showvideo vid={vi} />
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
 
 export default Showvideogrid
+
