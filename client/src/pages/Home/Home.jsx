3b87ecb28415c57f42f43bbe38e393c48d0021f5
sahil29roy <sahil29roy@gmail.com>
2025-01-06 20:58:30 +0530

    bug fix at home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Leftsidebar from '../../components/Leftsidebar/Leftsidebar'
-import Showvideogrid from '../../components/Showvideo/Showvideo'
 import "./Home.css"
+import Showvideogrid from '../../components/Showvideogrid/Showvideogrid'
 import vid from "../../components/Video/vid.mp4"
 const Home = () => {
-
   const vids = [
     {
       _id: 1,
       video_src: vid,
-      chanel: "Channel 1",
+      chanel: "wvjwenfj3njfwef",
       title: "video 1",
       uploader: "abc",
       description: "description of video 1"
@@ -17,7 +16,7 @@ const Home = () => {
     {
       _id: 2,
       video_src: vid,
-      chanel: "Channel 1",
+      chanel: "wvjwenfj3njfwef",
       title: "video 2",
       uploader: "abc",
       description: "description of video 2"
@@ -25,7 +24,7 @@ const Home = () => {
     {
       _id: 3,
       video_src: vid,
-      chanel: "Channel 1",
+      chanel: "wvjwenfj3njfwef",
       title: "video 3",
       uploader: "abc",
       description: "description of video 3"
@@ -33,13 +32,12 @@ const Home = () => {
     {
       _id: 4,
       video_src: vid,
-      chanel: "Channel 2",
+      chanel: "wvjwenfj3njfwef",
       title: "video 4",
       uploader: "xyz",
       description: "description of video 4"
     },
   ]
-
   const navlist = [
     "All",
     "Python",
@@ -62,7 +60,7 @@ const Home = () => {
             );
           })}
         </div>
-        <Showvideogrid vid={vids}/>
+        <Showvideogrid vid={vids} />
       </div>
     </div>
   )
