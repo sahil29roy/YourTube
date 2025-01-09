ab17723b5d0f1728bad4ecce575d1649c208fb74
sahil29roy <sahil29roy@gmail.com>
2025-01-10 03:14:35 +0530

    add display comment

--- client/src/components/Comment/Displaycomment.jsx
+++ client/src/components/Comment/Displaycomment.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React ,{useState}from 'react'
+import  './Comment.css'
+import moment from 'moment'
+const Displaycommment = ({cid,commentbody,userid,commenton,usercommented}) => {
+    const [edit,setedit]=useState(false)
+    const[cmtnody,setcommentbdy]=useState("")
+    const [cmtid,setcmntid]=useState("")
+    const currentuser={}
+    const handleedit=(ctid,ctbdy)=>{
+        setedit(true)
+        setcmntid(ctid)
+        setcommentbdy(ctbdy)
+    }
+    const haneleonsubmit=(e)=>{
+        // e.preventDefault();
+        if(!cmtnody){
+            alert("type your comment");
+        }else{
+            setcommentbdy("")
+        }
+        setedit(false)
+    }
+    // const handledel=(id)=>{
+    //     dispatch(deletecomment(id))
+    // }
 
-function Displaycomment() {
   return (
-    <div>
-      
-    </div>
+    <>
+    {edit?(
+        <>
+        <form  className="comments_sub_form_commments" onSubmit={haneleonsubmit() }>
+            <input type="text" onChange={(e)=>setcommentbdy(e.target.value)} placeholder='Edit comments..' value={cmtnody} className="comment_ibox" />
+            <input type="submit" value="change" className="comment_add_btn_comments" />
+        </form>
+        </>
+    ):(
+        <p className="comment_body">{commentbody}</p>
+    )}
+    <p className="usercommented">{" "}- {usercommented} commented {moment(commenton).fromNow()}</p>
+    {currentuser?.result?._id=== userid && (
+        <p className="EditDel_DisplayCommendt">
+            <i>Delete</i>
+            {/* <i onClick={()=>handleedit(cid,commentbody)}>Edit</i>
+            <i onClick={()=>handledel(cid)}>Delete</i> */}
+        </p>
+    )}
+    </>
   )
 }
 
-export default Displaycomment
+export default Displaycommment
