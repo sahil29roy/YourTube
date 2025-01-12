cbc665010397cc2d209f2fa58b31044d12e00ef0
sahil29roy <sahil29roy@gmail.com>
2025-01-12 20:22:18 +0530

    add video upload css

--- client/src/pages/Channel/Channel.jsx
+++ client/src/pages/Channel/Channel.jsx
@@ -4,7 +4,6 @@ import Leftsidebar from '../../Component/Leftsidebar/Leftsidebar'
 import Showvideogrid from '../../Component/Showvideogrid/Showvideogrid'
 import vid from "../../Component/Video/vid.mp4";
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 const Channel = ({seteditcreatechanelbtn,setvideouploadpage}) => {
   const {cid}=useParams()
       const vids = [
