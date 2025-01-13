9c21e8c1b3d5bb9afaf921ac3e62e0f908c59ba3
sahil29roy <sahil29roy@gmail.com>
2025-01-13 22:45:59 +0530

    add routes of liked videos

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Search from './pages/Search/Search'
 import Library from './pages/Library/Library'
+import Likedvideo from './Pages/Likedvideo/Likedvideo'
 import Channel from './pages/Channel/Channel'
 const AllRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AllRoutes = () => {
         <Route path='/search/:Searchquery' element={<Search/>}/>
         <Route path='/videopage/:vid' element={<Videopage/>}/>
         <Route path='/Library' element={<Library/>}/>
+        <Route path='/Likedvideo' element={<Likedvideo/>}/>
         <Route path='/channel/:cid' element={<Channel seteditcreatechanelbtn={seteditcreatechanelbtn} setvideouploadpage={setvideouploadpage}/>}/>
         </Routes>
     </div>
